552565e3a025cfcd416398a5f4dabfbd2c6a0d3f
Yohann <yoyo24du26@gmail.com>
2026-04-27 10:03:17 +0200

    feat(): - adding `fastify` package - removing `express` package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.setGlobalPrefix('api'); // All routes be prefixed by `/api/`
+  await app.listen(3000, '0.0.0.0'); // `0.0.0.0` for docker
 }
 bootstrap();
