759d8f59cce693ebe9584db05023dc28c15c3485
Yohann <yoyo24du26@gmail.com>
2026-04-27 10:24:22 +0200

    feat(backend): creating database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { buildTypeOrmConfig } from './config/typeorm.config';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        buildTypeOrmConfig(configService),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export const buildTypeOrmConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
+  type: 'mysql',
+  host: configService.get('DB_HOST') ?? 'localhost',
+  port: configService.get('DB_PORT') ?? 3306,
+  username: configService.get('DB_USER') ?? 'root',
+  password: configService.get('DB_PASSWORD') ?? 'pwd',
+  database: configService.get('DB_NAME') ?? 'database',
+  autoLoadEntities: true,
+  synchronize: configService.get('NODE_ENV') !== 'production',
+});
